29d67225d9611eaf6dca89856746f40a314b8835
황현민 <102218665+hyunmin200@users.noreply.github.com>
2022-09-15 22:36:15 +0900

    cpp 5일차
    비주얼 스튜디오의 단축키와 편의 사항 printf와 scanf의 사용법 함수에서 자료구조인 스택과 큐를 알아보았다.

--- MyStudyCpp/studyCpp2/main2.c
+++ MyStudyCpp/studyCpp2/main2.c
@@ -1,11 +1,10 @@
-#include <iostream>
 #include <stdio.h>
 
 int Add(int a, int b){
 	return a + b;
 }
 
-int main() {
+int main2() {
 	int iData = Add(100, 200);
 	
 	// 반복문
@@ -34,5 +33,37 @@ int main() {
 		printf("Output Test\n");
 	}
 
+
+	// 콘솔
+	// printf
+	printf("abcdef %d \n", 10);
+	printf("abcdef %f \n", 3.14f);
+
+	for (int i = 0; i < 10; ++i) {
+		printf("Output i : %d \n", i);
+	}
+
+	// scanf
+	int iInput = 0;
+	scanf("%d", &iInput);
+
+
+	// 함수
+	// 함수가 사용하는 메모리 영역
+	// 스택 메모리 영역
+
 	return 0;
 }
+
+// 단축키
+// 지정한 구문 주석					: Ctrl + k, c
+// 지정한 구문 주석 해제			: Ctrl + K, u
+// 원하는 구문만 드래그만 하기		: Alt Drag
+
+// 디버깅
+// 디버깅 시작 : F5
+// 다음 중단점까지 코드 실행 : F5 한 번 더 누르기
+// 디벙깅 중단점 생성 및 해제 : F9
+// 디버깅 중, 구문 수행			: F10
+// 디버깅 중, 구문 수행(햠수 진입) : F11
+// 디버깅 중, 디버깅 종료하기 : Shift + F5
