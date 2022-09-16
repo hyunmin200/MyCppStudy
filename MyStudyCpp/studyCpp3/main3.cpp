6be823dbcd0f2070e5655b970d101a9b9b17bc8d
황현민 <102218665+hyunmin200@users.noreply.github.com>
2022-09-16 23:31:10 +0900

    cpp 6일차
    함수를 더 공부하고 재귀함수에 대하여 공부를 하였다.

---
+++ MyStudyCpp/studyCpp3/main3.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//함수
+// Factorial
+int Factorial(int _iNum) 
+{
+	int iValue = 1;
+
+	for (int j = 0; j < _iNum - 1; ++j) 
+	{
+		iValue *= (j + 2);
+	}
+
+	Factorial(10);
+	
+	return iValue;
+}
+
+// 재귀함수
+// 가독성, 구현의 용이
+
+
+int main() {
+
+	int iValue = Factorial(4);
+
+	return 0;
+}
